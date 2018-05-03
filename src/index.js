f7f3092c3628fbc01b70ba36eb0cbaaf918e0e35
chiuwingyan <775039533@qq.com>
2018-05-03 16:33:35 +0800

    react热加载

--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Hello from './component/Hello/hello.jsx';
 import getRouter from './router/router'
-if(module.hot){
-    module.hot.accept();
+import { AppContainer } from 'react-hot-loader';
+//初始化
+renderWithHotReload(getRouter());
+//react-hot-loader热更新
+if (module.hot) {
+    module.hot.accept('./router/router', () => {
+        const getRouter = require('./router/router').default;
+        renderWithHotReload(getRouter());
+    });
 }
-ReactDom.render(
-    getRouter(),document.getElementById('app')
-);
+
+// if(module.hot){
+//     module.hot.accept();    //这是webpck-dev-server的模块热替换
+// }
+
+// ReactDom.render(
+//     getRouter(),document.getElementById('app')
+// );
+
+function renderWithHotReload(RootElement) {
+    ReactDom.render(
+        <AppContainer>
+            {RootElement}
+        </AppContainer>,
+        document.getElementById('app')
+    )
+}
